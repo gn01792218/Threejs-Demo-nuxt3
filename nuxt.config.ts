ecd75ea29043e01c5b1d775ce48593b24cb912d1
jacky <jacky@metallurgietech.com>
2022-12-02 16:22:30 +0800

    新增github倉庫名稱到設定檔

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,9 @@ import { defineNuxtConfig } from "nuxt";
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
     target:'static',
+    router: {
+        base: '/Threejs-Demo-nuxt3/'
+    },
     css: ["~/assets/css/style.css"],
     build:{
         postcss:{
