f463feae20d2c5eaa2c05d6af95dd570d991c48e
jacky <jacky@metallurgietech.com>
2022-12-07 09:54:09 +0800

    增加vite檔案資源解析的格式

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,6 @@ import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    target:'static',
-    router: {
-        base: '/Threejs-Demo-nuxt3/'
-    },
     css: ["~/assets/css/style.css"],
     build:{
         postcss:{
