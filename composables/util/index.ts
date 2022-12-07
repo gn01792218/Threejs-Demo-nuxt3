4bef3fe9c8003e32405296136d8884b96a2fffc5
jacky <jacky@metallurgietech.com>
2022-12-07 09:00:28 +0800

    刪除不必要檔案、增加useUtil說明

--- composables/util/index.ts
+++ composables/util/index.ts
@@ -1,5 +1,11 @@
 import useModelsUtil from "./useModelsUtil"
 import useImagesUtil from "./useImagesUtil"
+
+/**
+ * 工具集合入口
+ * 注意 : 各種取得資源的方法，如getModelsAssetsFileURL，無法在setup中使用，會出現500 server錯誤
+ * @returns 
+ */
 export default function useUtil() {
     const { getModelsAssetsFileURL } = useModelsUtil()
     const { getImagesAssetsFileURL } = useImagesUtil()
